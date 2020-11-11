c4e5e508c955be697bc2f06ea68327362175fc91
jiheyong <qmffn38@gmail.com>
2020-11-11 23:43:46 +0900

    vue sfc practice 1111

---
+++ vue/01_vue-cli/workshop/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>App</h1>
+    <input type="text" v-model="appData">
+    <p>appData:{{appData}}</p>
+    <p>parentData:{{parentData}}</p>
+    <p>childData: {{childData}}</p>
+    <Parent 
+      :appData="appData"
+      :parentData="parentData"
+      :childData="childData"
+      @onParentDataChange="onParentDataChange"
+      @onChildDataChange="onChildDataChange"
+    />
+  </div>
+</template>
+
+<script>
+import Parent from '@/components/Parent.vue'
+export default {
+  name:'App',
+  components:{
+    Parent,
+  },
+  data: function(){
+    return {
+      appData:'',
+      parentData:'',
+      childData:'',
+    }
+  },
+  methods:{
+    onParentDataChange: function(parentData){
+      this.parentData=parentData
+    },
+    onChildDataChange: function(childData){
+      this.childData=childData
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/01_vue-cli/workshop/src/components/Child.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Child</h1>
+    <input type="text" @keypress="onChange">
+    <p>appData: {{appData}}</p>
+    <p>parentData: {{parentData}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"Child",
+  props:{
+    appData:String,
+    parentData:String,
+    childData:String,
+  },
+  methods:{
+    onChange:function(event){
+      console.log(event.target.value)
+      this.$emit('onChildDataChange',event.target.value)
+    }
+  }
+}
+// 어떤 상황에서 자식 컴포넌트에 data를 쓰면 안되는가?
+// 자식에서 발생하는 data인데, 부모가 쓰거나, 혹은 형제가 사용한다.
+// 자식->부모로 data를 직접적으로 전달하는 것이 불가능
+
+// 해결법 : 부모에 data를 만든다.
+</script>
+
+<style>
+
+</style>
