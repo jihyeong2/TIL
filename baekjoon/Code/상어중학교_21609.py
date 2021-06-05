9f6ba9b5f5275f13e50cd057b0df82388862e8bc
jiheyong <qmffn38@gmail.com>
2021-06-05 21:58:03 +0900

    Update 상어중학교_21609.py

--- baekjoon/Code/상어중학교_21609.py
+++ baekjoon/Code/상어중학교_21609.py
@@ -1,48 +1,37 @@
 from collections import deque
+dx,dy=(-1,1,0,0),(0,0,-1,1)
 
-def bfs(i,j,val,visit):
-    global blockGroup,info
-
-    dx,dy=(-1,1,0,0),(0,0,-1,1)
-    visit[i][j]=1
+def bfs(i,j,val):
+    visit = [[0] * n for _ in range(n)]
     q=deque()
     q.append((i,j))
-    temp=[(i,j)]
-    t_stX,t_stY=i,j
-    cnt=0
-
+    visit[i][j]=1
+    group=[(i,j)]
     while q:
-        x,y=q.popleft()
-        if maps[x][y]==0:
-            cnt += 1
-        if maps[x][y]==val:
-            if x>t_stX:
-                t_stX,t_stY=x,y
-            elif x==t_stX and y>t_stY:
-                t_stY=y
-
+        x,y = q.popleft()
         for dir in range(4):
-            nx,ny=x+dx[dir],y+dy[dir]
-            if 0<=nx<n and 0<=ny<n and not visit[nx][ny] and maps[nx][ny]>=0:
-                if maps[nx][ny]==0 or maps[nx][ny]==val :
-                    q.append((nx,ny))
-                    visit[nx][ny]=1
-                    temp.append((nx,ny))
-    flag=False
-    if len(temp)<2: return
-    if len(temp)>len(blockGroup):
-        flag=True
-    elif len(temp)==len(blockGroup):
-        if info[0]>cnt:
-            flag=True
-        elif info[0]==cnt:
-            if info[1]<t_stX:
-                flag=True
-            elif info[1]==t_stX and info[2]<t_stY:
-                flag=True
-    if flag:
-        blockGroup = temp
-        info = [cnt, t_stX, t_stY]
+            nx,ny=x+dx[dir], y+dy[dir]
+            if 0<=nx<n and 0<=ny<n :
+                if visit[nx][ny]!=0 or maps[nx][ny]<0: continue
+                if maps[nx][ny]!=0 and maps[nx][ny]!=val: continue
+                q.append((nx,ny))
+                group.append((nx,ny))
+                visit[nx][ny]=1
+    if len(group) < 2:
+        return -1,-1,-1,-1
+
+    x,y,cnt = n,n,0
+    for a,b in group:
+        if maps[a][b] == 0:
+            cnt+=1
+        else:
+            if a<x:
+                x,y=a,b
+            elif a==x:
+                if b<y:
+                    x,y=a,b
+
+    return group,x,y,cnt
 
 def gravity():
     for j in range(n):
@@ -50,61 +39,62 @@ def gravity():
         for i in range(n):
             if maps[i][j]==-2: continue
             elif maps[i][j]==-1:
-                idx = i-1
-                while tmp:
-                    maps[idx][j] = tmp.pop()
-                    idx-=1
+                tmp.append(-1)
             else:
                 tmp.append(maps[i][j])
                 maps[i][j]=-2
-        idx=n-1
-        while tmp:
-            maps[idx][j]=tmp.pop()
-            idx-=1
+        flag=True
+        for i in range(n-1,-1,-1):
+            if len(tmp)<=0:
+                break
+            if flag==True:
+                val=tmp.pop()
+                if val == -1:
+                    flag=False
+            if flag == False:
+                if maps[i][j] == -1:
+                    flag=True
+                continue
+            maps[i][j]=val
+
 
 def rotate():
-    temp=[[0]*n for i in range(n)]
+    global maps
+    tmp_maps=[[0]*n for _ in range(n)]
     for i in range(n):
         for j in range(n):
-            temp[i][j]=maps[j][n-1-i]
-    return temp
-def show():
+            tmp_maps[i][j] = maps[j][n-1-i]
+    maps=tmp_maps
+
+n,m=map(int, input().split())
+maps=[list(map(int, input().split())) for _ in range(n)]
+result=0
+while True:
+    blockGroup,stX,stY,rainbowCnt = [],-1,-1,-1
     for i in range(n):
         for j in range(n):
-            print(maps[i][j],end=' ')
-        print()
-
-n,m=map(int,input().split())
-maps=[list(map(int, input().split())) for i in range(n)]
-
-blockGroup=[]
-info=[0,n,n]
-
-score=0
-while(True):
-    visit=[[0]*n for _ in range(n)]
-    for i in range(n):
-        for j in range(n):
-            if maps[i][j]<=0 or visit[i][j]==1: continue
-            bfs(i,j,maps[i][j],visit)
-    if len(blockGroup)==0: break
-    print(blockGroup)
-    score+=(len(blockGroup)**2)
-
+            if maps[i][j] > 0:
+                tmpGroup,tmpX,tmpY,tmpRainbowCnt = bfs(i,j,maps[i][j])
+                if tmpGroup == -1:
+                    continue
+                if len(tmpGroup) > len(blockGroup):
+                    blockGroup,stX,stY,rainbowCnt = tmpGroup,tmpX,tmpY,tmpRainbowCnt
+                elif len(tmpGroup) == len(blockGroup):
+                    if tmpRainbowCnt > rainbowCnt:
+                        blockGroup, stX, stY, rainbowCnt = tmpGroup, tmpX, tmpY, tmpRainbowCnt
+                    elif tmpRainbowCnt == rainbowCnt:
+                        if tmpX > stX:
+                            blockGroup, stX, stY, rainbowCnt = tmpGroup, tmpX, tmpY, tmpRainbowCnt
+                        elif tmpX == stX:
+                            if tmpY > stY:
+                                blockGroup, stX, stY, rainbowCnt = tmpGroup, tmpX, tmpY, tmpRainbowCnt
+    if len(blockGroup)==0:
+        break
+    result+=len(blockGroup)**2
     for x,y in blockGroup:
         maps[x][y]=-2
-    blockGroup.clear()
-    info = [0, n, n]
-    print('없애기')
-    show()
     gravity()
-    print('중력')
-    show()
-    maps=rotate()
-    print('회전')
-    show()
+    rotate()
     gravity()
-    print('중력2')
-    show()
 
-print(score)
+print(result)
